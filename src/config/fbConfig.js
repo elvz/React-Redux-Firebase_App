ddf2aca1a85a2163e9ae59880605a1beba412eb3
Alex <djelvz@gmail.com>
2019-05-16 14:27:58 +0300

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCYn8r2SNwG6XSwycikacxK_5zUjF9Uvfw",
+    apiKey: "YOUR_OWN_API_KEY",
     authDomain: "fozzy-wariors.firebaseapp.com",
     databaseURL: "https://fozzy-wariors.firebaseio.com",
     projectId: "fozzy-wariors",
     storageBucket: "fozzy-wariors.appspot.com",
-    messagingSenderId: "29343723880",
-    appId: "1:29343723880:web:a43c931ae0447d27"
+    messagingSenderId: "YOUR_MS_ID",
+    appId: "YOUR_APP_ID"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
